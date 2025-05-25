b8d26c00f3daf768ce5247553418fb0326eeb231
andrew0samir <andrewsamir317@gmail.com>
2025-05-25 23:43:55 +0300

    created hash function using SHA256

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
-    constructor(index, timestamp, date, previoushash = '') {
+    constructor(index, timestamp, data, previoushash = '') {
         this.index = index;
         this.timestamp = timestamp;
-        this.date = date;
+        this.data = data;
         this.previoushash = previoushash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }

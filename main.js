ea0aafd53e7db0dd62f07104e95d3f079e0b73e5
andrew0samir <andrewsamir317@gmail.com>
2025-06-15 23:54:01 +0300

    testing the transactions and addresses

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ class Blockchain {
         this.chain = [this.createGenesisBlock()];
 
         // this is the difficulty that control how many Zeros needed to be added to the hash 
-        this.difficulty = 4;
+        this.difficulty = 2;
 
         // this is responsable for saving the pending transactions between the intervals 
         // of mining blocks and added blocks into the blockchain
@@ -153,10 +153,18 @@ class Blockchain {
 
 }
 
-//  to test it :
-//      create instance 
-//      add new blocks
+// for testing the address and transactions
 let andrewCoin = new Blockchain();
 
+andrewCoin.createTransaction(new Transaction('address1', 'address2', 100));
+andrewCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
+console.log("\n starting the miner");
+andrewCoin.minePendingTransactions("andrew-address");
 
+console.log("\n balance of andrew is ", andrewCoin.getBalanceOfAddress("andrew-address"));
+
+console.log("\n starting the miner");
+andrewCoin.minePendingTransactions("andrew-address");
+
+console.log("\n balance of andrew is ", andrewCoin.getBalanceOfAddress("andrew-address"));

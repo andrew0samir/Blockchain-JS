3d30d0d1ee10e37391ec71b67d3eee119aa06dec
andrew0samir <andrewsamir317@gmail.com>
2025-06-22 23:11:24 +0300

    changed folder structure , cleaned the main file

--- src/blockchain.js
+++ src/blockchain.js
@@ -152,19 +152,3 @@ class Blockchain {
     }
 
 }
-
-// for testing the address and transactions
-let andrewCoin = new Blockchain();
-
-andrewCoin.createTransaction(new Transaction('address1', 'address2', 100));
-andrewCoin.createTransaction(new Transaction('address2', 'address1', 50));
-
-console.log("\n starting the miner");
-andrewCoin.minePendingTransactions("andrew-address");
-
-console.log("\n balance of andrew is ", andrewCoin.getBalanceOfAddress("andrew-address"));
-
-console.log("\n starting the miner");
-andrewCoin.minePendingTransactions("andrew-address");
-
-console.log("\n balance of andrew is ", andrewCoin.getBalanceOfAddress("andrew-address"));
